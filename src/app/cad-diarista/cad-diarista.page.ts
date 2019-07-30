83dc83cc48b36cd07b28b584c39ef75d6ee2e52e
mbfds <marcelo.florentino.ti@gmail.com>
2019-07-30 09:23:59 -0300

    imagem preview funcionando

--- src/app/cad-diarista/cad-diarista.page.ts
+++ src/app/cad-diarista/cad-diarista.page.ts
@@ -38,7 +38,7 @@ export class CadDiaristaPage implements OnInit {
    verGaleria(){
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
@@ -46,8 +46,8 @@ export class CadDiaristaPage implements OnInit {
   this.camera.getPicture(options).then((imageData) => {
    // imageData is either a base64 encoded string or a file URI
    // If it's base64 (DATA_URL):
-
    this.arquivo = 'data:image/jpeg;base64,' + imageData;
+
    console.log(this.arquivo);
   }, (err) => {
    // Handle error
@@ -59,14 +59,13 @@ export class CadDiaristaPage implements OnInit {
   takePhoto(){
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
   this.camera.getPicture(options).then((imageData) => {
    // imageData is either a base64 encoded string or a file URI
    // If it's base64 (DATA_URL):
-   this.img=imageData;
    this.arquivo = 'data:image/jpeg;base64,' + imageData;
    console.log(this.arquivo);
   }, (err) => {
